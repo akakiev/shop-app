a7db3d13f7e9db414e093fabdb26cbb5144c2906
Serhii Baraban <akakieva@gmail.com>
2023-02-24 10:17:53 +0100

    added the orders

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart.dart' show Cart;
 import '../widgets/cart_item.dart';
+import '../providers/orders.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -37,7 +38,13 @@ class CartScreen extends StatelessWidget {
                     ),
                     ElevatedButton(
                       child: Text('ORDER NOW'),
-                      onPressed: () {},
+                      onPressed: () {
+                        Provider.of<Orders>(context, listen: false).addOrder(
+                          cart.items.values.toList(),
+                          cart.totalAmount,
+                        );
+                        cart.clear();
+                      },
                     ),
                   ],
                 ),

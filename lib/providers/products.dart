aa32614c3d08641c3fee82683de415fb5f8338bf
Serhii Baraban <akakieva@gmail.com>
2023-03-23 17:47:37 +0100

    attached products to the users and added the filtering by the creator

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -72,9 +72,11 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  Future<void> fetchAndSetProducts() async {
+  Future<void> fetchAndSetProducts([bool filterByUser = false]) async {
+    final filterString =
+        filterByUser ? 'orderBy="creatorId"&equalTo="$userId"' : '';
     var url = Uri.parse(
-        'https://flutter-update-7c91d-default-rtdb.firebaseio.com/products.json?auth=$authToken');
+        'https://flutter-update-7c91d-default-rtdb.firebaseio.com/products.json?auth=$authToken&$filterString');
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -117,6 +119,7 @@ class Products with ChangeNotifier {
           'description': product.description,
           'imageUrl': product.imageUrl,
           'price': product.price,
+          'creatorId': userId,
           //'isFavorite': product.isFavorite,
         }),
       );
